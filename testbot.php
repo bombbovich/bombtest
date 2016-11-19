ad8a04576c6b41b7c7ca2a0663d4619c048e683f
bombbovich <surapun.tongrungsi@nectec.or.th>
2016-11-19 17:13:16 +0700

    Add files via upload

--- testbot.php
+++ testbot.php
@@ -1,14 +1,49 @@
 <?php
+$access_token = 'mTZICvfS9VFn9Y8SIZv1c2YEf8eqN1Ik9CNU+nYqN2J9G0K1F4cCFnf9MIMbs51Ib/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHOt8vJfcVrN8naBXP9zm72ZuUH7bPFXSYaIbbMFDkO/RAdB04t89/1O/w1cDnyilFU=';
 
-echo "test for first application";
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$i = 0;
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-for ($i=0;$i<=100;$i++){
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	echo "number = ";
-	echo $i;
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
+			echo $result . "\r\n";
+		}
+	}
 }
+echo "OK";
 
 ?>
