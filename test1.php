6abcef3e878cadc448d2e6c09d577b85e30d4c25
bombbovich <surapun.tongrungsi@nectec.or.th>
2016-11-22 15:14:53 +0700

    Add files via upload

--- test1.php
+++ test1.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-require "/vendor/autoload.php";
+require ".:/app/.heroku/php/lib/php/vendor/autoload.php";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ImmR4wvT8l5S+u/yTeY9rvGGLVLy46Yap6ant4nDKga0Rc1fxTm5aWJt0322dp8wb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPE6jA9/otIFPp7lEms20dB5/C5K7+QfvbPKSNNm9psRgdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'af688c7740b64fc4b521e78644f79a55']);
