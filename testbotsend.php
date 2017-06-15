854e6f8a068857322bc606a24d1f3c5371b1331b
bombbovich <surapun.tongrungsi@nectec.or.th>
2017-06-15 16:56:00 +0700

    Add files via upload

---
+++ testbotsend.php
@@ -0,0 +1,47 @@
+<?php
+$access_token = 'TNPzAFWAD9VBJjejExPpEjn00xmsDbOwuWrG8/QU0Rw+iAt0NvokuUlrNLYXrVcmb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPKRO/1fy0RZjq3P4OBnegQ4vs9I/ztLSCa6ws/3ytFMwdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:Sl341jGF275OLqY';
+
+$replytext = "";
+
+$replytext = "Push for test ^^";
+
+			$messages = [
+				'type' => 'text',
+				'text' => $replytext
+			];
+
+			$id = 'U924d3eaf86d1bc9e757633949f9ba23e';
+
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data2 = [
+				'to' => $id,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data2);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+echo "OK xx push";
+echo $result;
+
+?>
