bc55c8b5c6a604813751b1a5dc6a48f004fab332
bombbovich <surapun.tongrungsi@nectec.or.th>
2017-06-16 15:36:20 +0700

    Add files via upload

--- testbotsend.php
+++ testbotsend.php
@@ -14,6 +14,9 @@ print_r($_GET);
 
 $replytext = "";
 
+$now = new DateTime();
+echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
+
 if (isset($_GET)){
 	if (isset($_GET['alarm'])){
 		switch ($_GET['alarm']){
@@ -21,18 +24,18 @@ if (isset($_GET)){
 			case 2:$replytext = "Alarm CDA HIGH Pressure, cda pressure = ";break;
 			case 3:$replytext = "Alarm N2 LOW Pressure, N2 pressure = ";break;
 			case 4:$replytext = "Alarm N2 HIGH Pressure, N2 pressure = ";break;
-			case 5:$replytext = "Alarm PCH LOW Pressure, PCH pressure = ";break;
-			case 6:$replytext = "Alarm PCH HIGH Pressure, PCH pressure = ";break;
-			case 7:$replytext = "Alarm PCL LOW Pressure, PCL pressure = ";break;
-			case 8:$replytext = "Alarm PCL HIGH Pressure, PCL pressure = ";break;
-			case 9:$replytext = "Alarm PCH LOW Temperature, PCH Temp = ";break;
-			case 10:$replytext = "Alarm PCH HIGH Temperature, PCH Temp = ";break;
-			case 11:$replytext = "Alarm PCL LOW Temperature, PCL Temp = ";break;
-			case 12:$replytext = "Alarm PCL LOW Temperature, PCL Temp = ";break;
+			case 5:$replytext = "Alarm PCL LOW Pressure, PCH pressure = ";break;
+			case 6:$replytext = "Alarm PCL HIGH Pressure, PCH pressure = ";break;
+			case 7:$replytext = "Alarm PCH LOW Pressure, PCL pressure = ";break;
+			case 8:$replytext = "Alarm PCH HIGH Pressure, PCL pressure = ";break;
+			case 9:$replytext = "Alarm PCL LOW Temperature, PCH Temp = ";break;
+			case 10:$replytext = "Alarm PCL HIGH Temperature, PCH Temp = ";break;
+			case 11:$replytext = "Alarm PCH LOW Temperature, PCL Temp = ";break;
+			case 12:$replytext = "Alarm PCH HIGH Temperature, PCL Temp = ";break;
 		}
 	}
 	if (isset($_GET['data'])){
-		$replytext = $replytext . $_GET['data'];
+		$replytext = $now . $replytext . $_GET['data'];
 	}
 	echo $replytext;
 
