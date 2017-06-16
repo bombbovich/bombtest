51a31fba5f94e33fa107916a81b9d6eeb7fe86d8
bombbovich <surapun.tongrungsi@nectec.or.th>
2017-06-16 10:38:20 +0700

    Add files via upload

--- testbotsend.php
+++ testbotsend.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'TNPzAFWAD9VBJjejExPpEjn00xmsDbOwuWrG8/QU0Rw+iAt0NvokuUlrNLYXrVcmb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPKRO/1fy0RZjq3P4OBnegQ4vs9I/ztLSCa6ws/3ytFMwdB04t89/1O/w1cDnyilFU=';
+//$access_token = 'TNPzAFWAD9VBJjejExPpEjn00xmsDbOwuWrG8/QU0Rw+iAt0NvokuUlrNLYXrVcmb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPKRO/1fy0RZjq3P4OBnegQ4vs9I/ztLSCa6ws/3ytFMwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -7,42 +7,76 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:Sl341jGF275OLqY';
+//$proxy = 'velodrome.usefixie.com:80';
+//$proxyauth = 'fixie:Sl341jGF275OLqY';
 
 print_r($_GET);
 
 $replytext = "";
 
-$replytext = "Push for test ^^";
+if (isset($_GET)){
+	if (isset($_GET['alarm'])){
+		switch ($_GET['alarm']){
+			case 1:$replytext = "Alarm CDA LOW Pressure, cda pressure = ";break;
+			case 2:$replytext = "Alarm CDA HIGH Pressure, cda pressure = ";break;
+			case 3:$replytext = "Alarm N2 LOW Pressure, N2 pressure = ";break;
+			case 4:$replytext = "Alarm N2 HIGH Pressure, N2 pressure = ";break;
+			case 5:$replytext = "Alarm PCH LOW Pressure, PCH pressure = ";break;
+			case 6:$replytext = "Alarm PCH HIGH Pressure, PCH pressure = ";break;
+			case 7:$replytext = "Alarm PCL LOW Pressure, PCL pressure = ";break;
+			case 8:$replytext = "Alarm PCL HIGH Pressure, PCL pressure = ";break;
+			case 9:$replytext = "Alarm PCH LOW Temperature, PCH Temp = ";break;
+			case 10:$replytext = "Alarm PCH HIGH Temperature, PCH Temp = ";break;
+			case 11:$replytext = "Alarm PCL LOW Temperature, PCL Temp = ";break;
+			case 12:$replytext = "Alarm PCL LOW Temperature, PCL Temp = ";break;
+		}
+	}
+	if (isset($_GET['data'])){
+		$replytext = $replytext . $_GET['data'];
+	}
+	echo $replytext;
 
-$messages = [
-	'type' => 'text',
-	'text' => $replytext
-];
+	$id = 'U924d3eaf86d1bc9e757633949f9ba23e';//bombbovich
+	//$id = 'C3e2fa8fdacd3da78912d3c7524a1d7a2';//FEES Together
+	linesend($id,$replytext);
+}
 
-$id = 'U924d3eaf86d1bc9e757633949f9ba23e';//bombbovich
-//$id = 'C3e2fa8fdacd3da78912d3c7524a1d7a2';//FEES Together
+echo "Test Bot Send";
 
-$url = 'https://api.line.me/v2/bot/message/push';
-$data2 = [
-	'to' => $id,
-	'messages' => [$messages],
-];
-$post = json_encode($data2);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+function linesend($id,$replytext){
+	
+	$access_token = 'TNPzAFWAD9VBJjejExPpEjn00xmsDbOwuWrG8/QU0Rw+iAt0NvokuUlrNLYXrVcmb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPKRO/1fy0RZjq3P4OBnegQ4vs9I/ztLSCa6ws/3ytFMwdB04t89/1O/w1cDnyilFU=';
 
-echo $result . "\r\n";
+	$proxy = 'velodrome.usefixie.com:80';
+	$proxyauth = 'fixie:Sl341jGF275OLqY';
+
+
+	$messages = [
+		'type' => 'text',
+		'text' => $replytext
+	];
+
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$data2 = [
+		'to' => $id,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data2);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	echo $result . "\r\n";
+}
 
 ?>
