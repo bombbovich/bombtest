08b0215ba99944d31e81d7f7293e39e1e9d585df
bombbovich <surapun.tongrungsi@nectec.or.th>
2017-12-07 10:54:21 +0700

    Add files via upload

---
+++ profilegrouptest.php
@@ -0,0 +1,52 @@
+
+<?php
+
+$access_token = 'TNPzAFWAD9VBJjejExPpEjn00xmsDbOwuWrG8/QU0Rw+iAt0NvokuUlrNLYXrVcmb/zDCoAzwHgEWBWEJfDjDToHS7vu9KTnGuxeT/2yJHPKRO/1fy0RZjq3P4OBnegQ4vs9I/ztLSCa6ws/3ytFMwdB04t89/1O/w1cDnyilFU=';
+
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:Sl341jGF275OLqY';
+//http://fixie:Sl341jGF275OLqY@velodrome.usefixie.com:80
+///$servername = "203.185.96.48";
+//$username = "fees";
+//$password = "tmecfees01";
+//$dbname = "fees";
+
+// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+//if ($conn->connect_error) {
+  //  die("Connection failed: " . $conn->connect_error);
+//} 
+//echo "Connected successfully";
+
+//$sql = "SELECT * FROM line_bot";
+//$resultsql = $conn->query($sql);
+//echo $resultsql->num_rows;
+//if ($resultsql->num_rows > 0)
+//{	echo "inloop";
+//	while($row = $resultsql->fetch_assoc()) {
+//    	$textread1 = "UserId = ".$row['lineid']." and Status is ".$row['status']. " :end ";
+
+ //   	$url = 'https://api.line.me/v2/bot/profile/' . $row['lineid'];
+
+		$url = 'https://api.line.me/v2/bot/group/U88a83e8c7488364a843a6301948f938d/member/U924d3eaf86d1bc9e757633949f9ba23e';
+	echo $url;
+		
+		$headers = array('Authorization: Bearer ' . $access_token);
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		echo $result;
+//    }
+    echo $textread1;
+//}
+
+?>
