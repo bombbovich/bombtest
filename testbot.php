a845e446e4c6017329040543a261da998fc73ac6
bombbovich <surapun.tongrungsi@nectec.or.th>
2017-12-07 15:15:33 +0700

    Add files via upload

--- testbot.php
+++ testbot.php
@@ -19,7 +19,7 @@ $dbname = "fees";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+mysql_set_charset(UTF8);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
