eb9fcb16b7aee4f4e1a9c33b4c0245053ae0002b
bombbovich <surapun.tongrungsi@nectec.or.th>
2018-07-25 04:28:45 +0700

    Update testbotsend.php

--- testbotsend.php
+++ testbotsend.php
@@ -44,6 +44,12 @@ if (isset($_GET)){
 		$replytext = $replytext . $_GET['data'];
 	}
 	echo $replytext;
+	
+	
+	
+	
+		
+		cho $replytext;
 
 	$id = 'U924d3eaf86d1bc9e757633949f9ba23e';//bombbovich
 	//$id = 'C3e2fa8fdacd3da78912d3c7524a1d7a2';//FEES Together
@@ -54,7 +60,7 @@ if (isset($_GET)){
 	linesend($id,$replytext);
 
 	$id = 'Cfe7e00a2ef082a3a295ae8c231152d47';
-	linesend($id,$replytext);
+	linesend($id,$replytext*/
 }
 
 echo "Test Bot Send";
